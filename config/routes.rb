6b9b3e0c974dbaa629d722ec7e0efe0917af65fe
elaraby elaidy <elarabyelaidy@gmail.com>
2021-10-05 23:21:13 +0200

    prepare to deploy

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cats, except: :destroy 
 
-  get "cats/", to: "cats#index" 
-  get "cats/:id", to: "cats#show" 
-  get "cats/new", to: "cats#new"
   
 =begin 
   get "cats/", path"cats/index"  
